bd3134182e4eb6dab8643894abbb2c227c6c385b
jimmsi <jimmyem@gmail.com>
2024-02-12 15:45:16 +0100

    Added Customer class

--- src/main/java/se/yrgo/Customer.java
+++ src/main/java/se/yrgo/Customer.java
@@ -1,4 +1,51 @@
 package se.yrgo;
 
+import java.util.ArrayList;
+
 public class Customer {
+
+    private int customerID;
+    private String name;
+    private String adress;
+    private String email;
+    private ArrayList<Account> accountList;
+
+    public Customer(int customerID, String name, String adress, String email) {
+        this.customerID = customerID;
+        this.name = name;
+        this.adress = adress;
+        this.email = email;
+        this.accountList = null;
+    }
+
+    public void addAccount (Account account) {
+        if (this.accountList == null) {
+            this.accountList = new ArrayList<>(); // Skapa listan första gången ett konto läggs till
+        }
+        this.accountList.add(account);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Customer {");
+        sb.append("customerID = ").append(customerID);
+        sb.append(", name = '").append(name).append('\'');
+        sb.append(", adress = '").append(adress).append('\'');
+        sb.append(", email = '").append(email).append('\'');
+        sb.append(", accountList = [");
+
+        for (int i = 0; i < accountList.size(); i++) {
+            sb.append(accountList.get(i).toString());
+            if (i < accountList.size() - 1) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append("]}");
+        return sb.toString();
+    }
+
 }
+
+
--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -5,8 +5,14 @@ import static se.yrgo.Transaction.transactionList;
 public class Main {
     public static void main(String[] args) {
         //Nya konton
+
         Account acc1 = new Account("23534643", 800);
         Account acc2 = new Account("23534642", 1000);
+
+        Customer cust = new Customer(1, "Jimmy", "Gatan 1", "jim@gmail.com");
+        cust.addAccount(acc1);
+        cust.addAccount(acc2);
+
         //Nya transaktioner
         Transaction t1 = new Transaction("2024-02-12", "withdraw", 400, acc1);
         Transaction t2 = new Transaction("2024-02-12", "deposit", 300, acc1);
@@ -19,6 +25,8 @@ public class Main {
         for (Transaction transaction : transactionList) {
             System.out.println(transaction);
         }
+
+        System.out.println(cust);
 
     }
 }
