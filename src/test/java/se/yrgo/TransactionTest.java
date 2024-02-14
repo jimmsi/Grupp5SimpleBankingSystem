a568c0f3b2a65424390b5b5c45968159a3fbe6bd
daniel-svendsen <daniel-svendsen@hotmail.se>
2024-02-14 08:38:09 +0100

    added tests

--- src/test/java/se/yrgo/TransactionTest.java
+++ src/test/java/se/yrgo/TransactionTest.java
@@ -17,5 +17,31 @@ public class TransactionTest {
         assertEquals(700.0, account2.getBalance(), 0.001);
     }
 
+    @Test
+    public void testException() {
+        Account account1 = new Account("123456", 1000.0);
+        Account account2 = new Account("789012", 500.0);
+        Account account3 = null;
+
+        // Attempting to create a transaction with a negative amount should throw an IllegalArgumentException
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Transaction("2024-02-12", "Transfer", -200.0, account1, account2);
+        });
+        // Attempting to create a transaction with null source account should also throw an IllegalArgumentException
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Transaction("2024-02-12", "Transfer", -200.0, account3, account3);
+        });
+    }
+
+    @Test
+    public void testInsufficientFundsException() {
+        Account account1 = new Account("123456", 1000.0);
+        Account account2 = new Account("789012", 500.0);
+
+        // Attempting to create a transaction with an amount greater than the balance of the source account should throw an IllegalStateException
+        assertThrows(IllegalStateException.class, () -> {
+            new Transaction("2024-02-12", "Transfer", 1500.0, account1, account2);
+        });
+    }
 
 }
